e9ffb932b6d8a4f7b19d7a8bba12ec38d8623917
Shakalych <madstabr@gmail.com>
2022-09-19 18:10:26 +0500

    3 Задача

---
+++ Exam3/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] array1 = FillArray(2, 2);
+int[,] array2 = FillArray(2, 2);
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
+int[,] resultArray = new int[2,2];
+Console.WriteLine();
+PrintArray(Matrix(array1, array2, resultArray));
+
+
+int[,] FillArray(int m, int n)
+{
+	int[,] array = new int[m, n];
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			array[i, j] = new Random().Next(0, 10);
+		}
+	}
+	return array;
+}
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write(array[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+int[,] Matrix(int[,] array1, int[,]array2, int[,] resulArray)
+{
+    for (int i = 0; i < resultArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultArray.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                sum += array1[i,k] * array2[k,j];
+            }
+            resultArray[i,j] = sum;
+        }
+    }
+    return resulArray;
+}
+
